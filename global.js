f6cc7d31b2d0ea7476f0e7982d707bcab2db14b6
Chintan Jain <79692092+chintanjain137@users.noreply.github.com>
2024-06-18 11:24:12 +0530

    update global.js

---
+++ global.js
@@ -0,0 +1,20 @@
+Update your global.js file
+## find onVariantChange()
+ -- Add below line of code just above
+"if(!this.currentVariant)"
+this.filterImgVariant();
+
+-- Add below function definination after the onVariantChange() function
+ defination:
+```
+filterImgVariant() {
+  if(this.currentVariant.featured_media && this.currentVariant.featured_media.alt) {
+      document.querySelectorAll('[thumbnail-alt]').forEach(img => img.style.display = 'none')
+      const currentImgAlt = this.currentVariant.featured_media.alt
+      const thumbnailSelector = `[thumbnail-alt = '${currentImgAlt}']`
+      document.querySelectorAll(thumbnailSelector).forEach(img => img.style.display = 'block')
+  } else {
+      document.querySelectorAll('[thumbnail-alt]').forEach(img => img.style.display = 'block')
+  }
+} 
+```
